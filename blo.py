44fe8798e803b0b57a8e52e02f29fedc67664e09
Spencer Alves <impiaaa@gmail.com>
2017-02-27 05:17:48 +0000

    Imported from Dropbox blo.py at Mon, 27 Feb 2017 05:17:48 +0000

--- blo.py
+++ blo.py
@@ -1,7 +1,51 @@
 import sys, os
-from struct import pack, unpack
-import struct
+from struct import pack, unpack, Struct, error as StructError
 from warnings import warn
+from common import Section, BFile
+
+class Bgn1(Section): pass
+
+class End1(Section): pass
+
+class Inf1(Section):
+    header = Struct('>HH4x')
+    def read(self, fin, start, size):
+        self.width, self.height = self.header.unpack(fin.read(size-8))
+
+class Pic1(Section):
+    header = Struct('>BBBB4s')
+    def read(self, fin, start, size):
+        u = [None]*10
+        u[0], u[1], u[2], u[4], id = self.header.unpack(fin.read(8))
+        c = 8
+        if u[0] == 0x06:
+            # relative to parent
+            x, y, width, height = unpack(">hhhh", data[8:16])
+            c = 16
+        elif u[0] == 0x07:
+            # relative to last
+            x, y, width, height, u[4], u[5], u[6], u[7] = unpack(">xbxbhhBBBB", data[8:20])
+            c = 20
+            x += lastX
+            y += lastY
+        elif u[0] == 0x08:
+            # relative to parent, but different order, and set last
+            x, y, width, height, u[4], u[5], u[6], u[7] = unpack(">hhhhBBBB", data[8:20])
+            c = 20
+            lastX = x
+            lastY = y
+        elif u[0] == 0x09:
+            # ???
+            x, y, width, height, u[4], u[5], u[6], u[7] = unpack(">hhhhBBBB", data[8:20])
+            c = 20
+        else:
+            raise Exception("Unknown rel type 0x%02X"%u[0])
+        u[8], u[9], namelen = unpack(">BBB", data[c:c+3])
+        c += 3
+        name = os.path.splitext(data[c:c+namelen])[0]
+        c += namelen
+        c += 4-(c%4)
+        u += map(ord, data[c:])
 
 fin = open(sys.argv[1], 'rb')
 signature, fileLength, chunkCount, svr = unpack('>8sLL4s12x', fin.read(0x20))
@@ -15,7 +59,7 @@ grayimages = ["coin_back", "error_window", "juice_liquid", "juice_mask", "juice_
 for chunkno in xrange(chunkCount):
     start = fin.tell()
     try: chunk, size = unpack('>4sL', fin.read(8))
-    except struct.error:
+    except StructError:
         warn("File too small for chunk count of "+str(chunkCount))
         continue
     chunks.append((chunk, fin.read(size-8)))
@@ -34,10 +78,10 @@ def parsechunks(chunklist, i=0, indent=0, parentx=0, parenty=0):
         elif chunk == "END1":
             return i
         elif chunk == "INF1":
-            width, height = unpack('>HH4x', data)
+            width, height = unpack(, data)
         elif chunk == "PIC1":
             u = [None]*10
-            u[0], u[1], u[2], u[4], id = unpack(">BBBB4s", data[:8])
+            u[0], u[1], u[2], u[4], id = unpack("", data[:8])
             c = 8
             if u[0] == 0x06:
                 # relative to parent
