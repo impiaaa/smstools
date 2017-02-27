9857cbfac123040e4d717083ab30b45aa1b9564b
Spencer Alves <impiaaa@gmail.com>
2017-02-27 05:26:29 +0000

    Imported from Dropbox blo.py at Mon, 27 Feb 2017 05:26:29 +0000

--- blo.py
+++ blo.py
@@ -18,16 +18,12 @@ class Pic1(Section):
         u = [None]*9
         self.rel, u[0], u[1], u[2], self.id = self.header.unpack(fin.read(8))
         if self.rel == 0x06:
-            # relative to parent
             self.x, self.y, self.width, self.height = unpack(">hhhh", fin.read(8))
         elif self.rel == 0x07:
-            # relative to last
             self.x, self.y, self.width, self.height, u[3], u[4], u[5], u[6] = unpack(">xbxbhhBBBB", fin.read(12))
         elif self.rel == 0x08:
-            # relative to parent, but different order, and set last
             self.x, self.y, self.width, self.height, u[3], u[4], u[5], u[6] = unpack(">hhhhBBBB", fin.read(12))
         elif self.rel == 0x09:
-            # ???
             self.x, self.y, self.width, self.height, u[3], u[4], u[5], u[6] = unpack(">hhhhBBBB", fin.read(12))
         else:
             raise Exception("Unknown rel type 0x%02X"%self.rel)
@@ -66,39 +62,21 @@ def parsechunks(chunklist, i=0, indent=0, parentx=0, parenty=0):
         elif chunk == "END1":
             return i
         elif chunk == "INF1":
-            width, height = unpack(, data)
         elif chunk == "PIC1":
-            u = [None]*10
-            u[0], u[1], u[2], u[4], id = unpack("", data[:8])
-            c = 8
             if u[0] == 0x06:
                 # relative to parent
-                x, y, width, height = unpack(">hhhh", data[8:16])
-                c = 16
             elif u[0] == 0x07:
                 # relative to last
-                x, y, width, height, u[4], u[5], u[6], u[7] = unpack(">xbxbhhBBBB", data[8:20])
-                c = 20
                 x += lastX
                 y += lastY
             elif u[0] == 0x08:
                 # relative to parent, but different order, and set last
-                x, y, width, height, u[4], u[5], u[6], u[7] = unpack(">hhhhBBBB", data[8:20])
-                c = 20
                 lastX = x
                 lastY = y
             elif u[0] == 0x09:
                 # ???
-                x, y, width, height, u[4], u[5], u[6], u[7] = unpack(">hhhhBBBB", data[8:20])
-                c = 20
             else:
                 raise Exception("Unknown rel type 0x%02X"%u[0])
-            u[8], u[9], namelen = unpack(">BBB", data[c:c+3])
-            c += 3
-            name = os.path.splitext(data[c:c+namelen])[0]
-            c += namelen
-            c += 4-(c%4)
-            u += map(ord, data[c:])
             htmlout.write('<img style="position:absolute; left:%dpx; top:%dpx; width:%dpx; height: %dpx; border: black 0px solid" src="../timg/%s.png" id="%s">\n'%(x,y,width,height,name,id))
         elif chunk == "PAN1":
             unknown1, id, x, y, width, height = unpack(">H2x4shhhh", data[:16])
