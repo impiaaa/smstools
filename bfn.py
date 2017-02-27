d50eaf4c48d9ff4aa948712f116914304454dbe8
Spencer Alves <impiaaa@gmail.com>
2017-02-27 04:20:20 +0000

    Imported from Dropbox bfn.py at Mon, 27 Feb 2017 04:20:20 +0000

--- bfn.py
+++ bfn.py
@@ -11,11 +11,39 @@ class Gly1(Section):
 	header = struct.Struct('>H4xHH')
 	def read(self, fin, start, size):
         fin.seek(0xC, 1)
-        format, w, h = self.header.unpack(fin.read(0xa))
-        h = (size-0x18)/w
-        if format == 0: h *= 2
+        self.format, self.w, self.h = self.header.unpack(fin.read(0xa))
+        self.h = (size-0x18)/w
+        if format == 0: self.h *= 2
         fin.seek(2, 1)
-        self.data = 
+        self.data = fin.read(size-0x18)
+	def export(self, name):
+		i = 0
+		if bfn.gly1.format == 0:
+			im = Image.new('L', (w, h))
+			for y in range(0, h, 8):
+				for x in range(0, w, 8):
+					for dy in range(8):
+						for dx in range(0, 8, 2):
+							c = ord(bfn.gly1.data[i])
+							i += 1
+							if x + dx < w and y + dy < h:
+								t = c&0xF0
+								im.putpixel((x+dx, y+dy), t | (t >> 4))
+								t = c&0x0F
+								im.putpixel((x+dx+1, y+dy), (t << 4) | t)
+		elif bfn.gly1.format == 2:
+			im = Image.new('RGBA', (w, h))
+			for y in xrange(0, h, 4):
+				for x in xrange(0, w, 8):
+					for dy in xrange(4):
+						for dx in xrange(0, 8):
+							c = ord(fin.read(1))
+							if x + dx < w and y + dy < h:
+								t = c&0xF0
+								#im.putpixel((x+dx, y+dy), (t | (t >> 4),)*3)
+								a = c&0x0F
+								im.putpixel((x+dx, y+dy), (t | (t >> 4),)*3+((a << 4) | a,))
+		im.save(name+'.png')
 
 class BFont(BFile):
     def __init__(self, *args, **kwargs):
@@ -31,31 +59,4 @@ fin = open(sys.argv[1], 'rb')
 bfn = BFont()
 bfn.read(fin)
 fin.close()
-
-i = 0
-if bfn.gly1.format == 0:
-	im = Image.new('L', (w, h))
-	for y in range(0, h, 8):
-		for x in range(0, w, 8):
-			for dy in range(8):
-				for dx in range(0, 8, 2):
-					c = ord(bfn.gly1.data[i])
-					i += 1
-					if x + dx < w and y + dy < h:
-						t = c&0xF0
-						im.putpixel((x+dx, y+dy), t | (t >> 4))
-						t = c&0x0F
-						im.putpixel((x+dx+1, y+dy), (t << 4) | t)
-elif bfn.gly1.format == 2:
-	im = Image.new('RGBA', (w, h))
-	for y in xrange(0, h, 4):
-		for x in xrange(0, w, 8):
-			for dy in xrange(4):
-				for dx in xrange(0, 8):
-					c = ord(fin.read(1))
-					if x + dx < w and y + dy < h:
-						t = c&0xF0
-						#im.putpixel((x+dx, y+dy), (t | (t >> 4),)*3)
-						a = c&0x0F
-						im.putpixel((x+dx, y+dy), (t | (t >> 4),)*3+((a << 4) | a,))
-im.save(sys.argv[1]+'.png')
+bfn.gly1.export(sys.argv[1])
