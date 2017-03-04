e4a444393980ddd296a166cd37f46cf6a97937b8
Spencer Alves <impiaaa@gmail.com>
2017-03-04 23:27:38 +0000

    Imported from Dropbox texture.py at Sat, 04 Mar 2017 23:27:38 +0000

--- texture.py
+++ texture.py
@@ -1,13 +1,39 @@
+GX_TF_I4 = 0x0
+GX_TF_I8 = 0x1
+GX_TF_IA4 = 0x2
+GX_TF_IA8 = 0x3
+GX_TF_RGB565 = 0x4
+GX_TF_RGB5A3 = 0x5
+GX_TF_RGBA8 = 0x6
+GX_TF_C4 = 0x8
+GX_TF_C8 = 0x9
+GX_TF_C14X2 = 0xA
+GX_TF_CMPR = 0xE # S3TC/DXT
+
 formatBytesPerPixel = {
- 0: .5, # I4
- 1:  1, # I8
- 2:  1, # IA4
- 3:  2, # IA8
- 4:  2, # RGB565
- 5:  1, # RGB5A3
- 6:  1, # RGBA8
- 8: .5, # C4
- 9:  1, # C8
-10:  2, # C14X2
-14: .5  # CMPR (S3TC/DXT)
+GX_TF_I4:   0.5,
+GX_TF_I8:     1,
+GX_TF_IA4:    1,
+GX_TF_IA8:    2,
+GX_TF_RGB565: 2,
+GX_TF_RGB5A3: 1,
+GX_TF_RGBA8:  1,
+GX_TF_C4:   0.5,
+GX_TF_C8:     1,
+GX_TF_C14X2:  2,
+GX_TF_CMPR: 0.5
+}
+
+formatBlockWidth = {
+GX_TF_I4: 8,
+GX_TF_I8: 8,
+GX_TF_IA4: 8,
+GX_TF_IA8: 4,
+GX_TF_RGB565: 4,
+GX_TF_RGB5A3: 4,
+GX_TF_RGBA8: 4,
+GX_TF_C4: 8,
+GX_TF_C8: 8,
+GX_TF_C14X2: 4,
+GX_TF_CMPR: 8
 }
