1d4495c99bb2d1553d58a54c4a89896234c66734
Spencer Alves <impiaaa@gmail.com>
2017-03-04 23:22:43 +0000

    Imported from Dropbox texture.py at Sat, 04 Mar 2017 23:22:43 +0000

---
+++ texture.py
@@ -0,0 +1,13 @@
+formatBytesPerPixel = {
+ 0: .5, # I4
+ 1:  1, # I8
+ 2:  1, # IA4
+ 3:  2, # IA8
+ 4:  2, # RGB565
+ 5:  1, # RGB5A3
+ 6:  1, # RGBA8
+ 8: .5, # C4
+ 9:  1, # C8
+10:  2, # C14X2
+14: .5  # CMPR (S3TC/DXT)
+}
