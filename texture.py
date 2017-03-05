8e58bfa5e8c19fa5fe97e679eda409fd4667f5b1
Spencer Alves <impiaaa@gmail.com>
2017-03-05 05:01:28 +0000

    Imported from Dropbox texture.py at Sun, 05 Mar 2017 05:01:28 +0000

--- texture.py
+++ texture.py
@@ -234,7 +234,7 @@ def readTextureData(fin, format, width, height, mipmapCount=1, arrayCount=1):
     # data length = sum from i=0 to mipCount of (w*h/(4^i))
     mipSize = calcTextureSize(format, width, height)
     sliceSize = int(mipSize*(4-4**(1-mipmapCount))/3)
-    raise Exception(mipSize, sliceSize, struct.calcsize(data.typecode))
+    print(mipSize, sliceSize, struct.calcsize(data.typecode))
     data.fromfile(fin, int(arrayCount*sliceSize/struct.calcsize(data.typecode)))
     return data
 
