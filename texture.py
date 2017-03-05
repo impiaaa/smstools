5d46dc56e1939b39a8cbf9c9c5758eedab6c443b
Spencer Alves <impiaaa@gmail.com>
2017-03-05 03:43:38 +0000

    Imported from Dropbox texture.py at Sun, 05 Mar 2017 03:43:38 +0000

--- texture.py
+++ texture.py
@@ -229,7 +229,6 @@ def readTextureData(fin, format, width, height, mipmapCount=1, arrayCount=1):
     # data length = sum from i=0 to mipCount of (w*h/(4^i))
     mipSize = calcTextureSize(format, width, height)
     sliceSize = int(mipSize*(4-4**(1-mipmapCount))/3)
-    print format, width, height, mipSize, arrayCount, sliceSize, struct.calcsize(data.typecode)
     data.fromfile(fin, arrayCount*sliceSize/struct.calcsize(data.typecode))
     return data
 
