dda0339150412c86e4345e6b06d8e8d893e47836
Spencer Alves <impiaaa@gmail.com>
2017-03-05 03:44:22 +0000

    Imported from Dropbox texture.py at Sun, 05 Mar 2017 03:44:22 +0000

--- texture.py
+++ texture.py
@@ -229,7 +229,7 @@ def readTextureData(fin, format, width, height, mipmapCount=1, arrayCount=1):
     # data length = sum from i=0 to mipCount of (w*h/(4^i))
     mipSize = calcTextureSize(format, width, height)
     sliceSize = int(mipSize*(4-4**(1-mipmapCount))/3)
-    data.fromfile(fin, arrayCount*sliceSize/struct.calcsize(data.typecode))
+    data.frombytes(fin.read(arrayCount*sliceSize))
     return data
 
 def decodeTexturePIL(data, format, width, height, paletteFormat=None, palette=None, mipmapCount=1, arrayCount=1):
