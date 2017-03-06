a7829c8f186e6147a2fd9dcf29a55c3885da56bc
Spencer Alves <impiaaa@gmail.com>
2017-03-06 05:00:03 +0000

    Imported from Dropbox texture.py at Mon, 06 Mar 2017 05:00:03 +0000

--- texture.py
+++ texture.py
@@ -268,7 +268,6 @@ def calcTextureSize(format, width, height):
     blockHeight = formatBlockHeight[format]
     fullWidth = int((float(width)/blockWidth)+0.5)*blockWidth
     fullHeight = int((float(height)/blockHeight)+0.5)*blockHeight
-    print width, height, blockWidth, blockHeight, fullWidth, fullHeight
     return int(fullWidth*fullHeight*formatBytesPerPixel[format])
 
 def readTextureData(fin, format, width, height, mipmapCount=1, arrayCount=1):
