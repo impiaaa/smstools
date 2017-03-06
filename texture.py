cc2490adbe02cefa48cdb563299bf029e164dc1a
Spencer Alves <impiaaa@gmail.com>
2017-03-06 04:17:32 +0000

    Imported from Dropbox texture.py at Mon, 06 Mar 2017 04:17:32 +0000

--- texture.py
+++ texture.py
@@ -373,8 +373,8 @@ ddsFormats = {
     GX_TF_IA4:    (DDPF_ALPHAPIXELS|DDPF_LUMINANCE, '',      8,       0xF0,          0,          0,       0x0F),
     GX_TF_IA8:    (DDPF_ALPHAPIXELS|DDPF_LUMINANCE, '',     16,     0x00FF,          0,          0,     0xFF00),
     GX_TF_RGB565: (DDPF_RGB,                        '',     16,     0xF800,     0x07E0,     0x001F,          0),
-    GX_TF_RGB5A3: (DDPF_ALPHAPIXELS|DDPF_RGB,       '',     32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000),
-    GX_TF_RGBA8:  (DDPF_ALPHAPIXELS|DDPF_RGB,       '',     32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000),
+    GX_TF_RGB5A3: (DDPF_ALPHAPIXELS|DDPF_RGB,       '',     32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000),
+    GX_TF_RGBA8:  (DDPF_ALPHAPIXELS|DDPF_RGB,       '',     32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000),
     GX_TF_CMPR:   (DDPF_ALPHAPIXELS|DDPF_FOURCC,    'DXT1',  0,          0,          0,          0,          0)
 }
 ddsPaletteFormats = {
