1cf5c8f5e5c7c2eba3a809b197f2703b3d4d62cc
Spencer Alves <impiaaa@gmail.com>
2023-02-28 13:59:12 -0800

    ymap: field name updates (thanks Halleester)

--- ymap.py
+++ ymap.py
@@ -10,28 +10,24 @@ if len(sys.argv) != 2:
 
 fin = open(sys.argv[1], 'rb')
 
-nRegions, zero1, dataOffset = unpack('>HHI', fin.read(8))
+nRegions, dataOffset = unpack('>H2xI', fin.read(8))
 
 assert dataOffset == 8
-assert zero1 == 0
 
 fin.seek(dataOffset)
 
 print("%d regions"%nRegions)
 
 for i in range(nRegions):
-    pollutionEffect, unk1, pollutionLayerType = unpack('>HHI', fin.read(8))
-
-    #assert sz == 0x20000, hex(fin.tell())
-    #assert zero2 == 0, hex(fin.tell())
+    pollutionEffect, flags, pollutionLayerType = unpack('>HHH2x', fin.read(8))
 
     yOffset, texToWorldSize, xMin, zMin, xMax, zMax = unpack('>ffffff', fin.read(24))
 
-    widthPow, heightPow, unk3, dataOffset = unpack('>HHII', fin.read(12))
+    widthPow, heightPow, unk, dataOffset = unpack('>HHII', fin.read(12))
     width = 1<<widthPow
     height = 1<<heightPow
 
-    print("(%f,%f),(%f,%f),(%f,%f) 0x%X"%(yOffset, texToWorldSize, xMin, zMin, xMax, zMax, unk3))
+    print("(%f,%f),(%f,%f),(%f,%f) 0x%X"%(yOffset, texToWorldSize, xMin, zMin, xMax, zMax, unk))
     
     lastRegionHeader = fin.tell()
     
@@ -39,7 +35,7 @@ for i in range(nRegions):
     data = readTextureData(fin, TexFmt.I8, width, height)
     
     images = decodeTexturePIL(data, TexFmt.I8, width, height, 0, None)
-    images[0][0].save(os.path.splitext(sys.argv[1])[0]+"-%d-%X.png"%(i, unk3))
+    images[0][0].save(os.path.splitext(sys.argv[1])[0]+"-%d-%X.png"%(i, unk))
     
     fin.seek(lastRegionHeader)
 
